9484befaa36561d0a569747d18b4bb5d3ba54b8c
kitaa <kedama.mario@gmail.com>
2021-03-01 06:38:27 +0000

    header-blade-login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,12 +13,16 @@
                 <a class="navbar-brand" href="/">Tasklist</a>
 
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                    <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="nav-bar">
                     <ul class="navbar-nav mr-auto"></ul>
                     <ul class="navbar-nav">
+                        {{-- ユーザ登録ページへのリンク --}}
+                        <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                        {{-- ログインページへのリンク --}}
+                        <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
                     </ul>
                 </div>
             </nav>
